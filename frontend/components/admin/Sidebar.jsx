213934a0f9345c32d770affd098e29ff2f4c507d
srikanth32145 <srikanth@hirolainfotech.com>
2023-05-15 10:09:02 +0530

    updated frontend folder in the reopsitory

---
+++ frontend/components/admin/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+<div className="sidebar-menu">
+    <div className="sidebar-header">
+        <h3 className="sidebar">sidebatr</h3>
+    </div>
+</div>
+    </>
+  )
+}
+
+export default Sidebar
